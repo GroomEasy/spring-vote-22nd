3488d6dfae472872f2a35a9bbf9ae72602d08cc8
이지원 <daggero@naver.com>
2025-11-16 23:02:36 +0900

    Initial commit

---
+++ src/test/java/com/ceos/springvote22nd/SpringVote22ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos.springvote22nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVote22ndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
