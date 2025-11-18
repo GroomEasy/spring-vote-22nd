677032a9d8d5eb37f51630fd012a1cca7fea3bd3
이지원 <daggero@naver.com>
2025-11-19 03:15:53 +0900

    refactor: security config에 swagger 경로 추가

--- src/main/java/com/ceos/springvote22nd/global/config/SecurityConfig.java
+++ src/main/java/com/ceos/springvote22nd/global/config/SecurityConfig.java
@@ -37,7 +37,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
-                                "/api/user/signup"
+                                "/api/user/signup",
+
+                                // Swagger
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
